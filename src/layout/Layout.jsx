601d5ff97f4f7b65aa656c31e2f4efd617bd7f8a
tubagusivan <ivanbudiman.tb@gmail.com>
2023-06-15 11:02:11 +0700

    feat: add navbar

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
     return (
         <>
+            <div>
+                <Navbar />
+            </div>
             <div>
                 <Outlet />
             </div>
